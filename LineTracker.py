53436eefdeefa43b728acd65a313c999a78235ad
vbertalan <vbertalan@gmail.com>
2024-04-03 17:22:32 -0400

    new commit

--- LineTracker.py
+++ LineTracker.py
@@ -14,6 +14,7 @@ import linetracker.line_distance as ld
 import linetracker.parser.parser as p
 import linetracker.main as m
 
+
 from rich.console import Console
 from tqdm import tqdm
 import numpy as np
